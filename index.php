68bfd611e1342a729bc5f8e9936ddf48775e2a11
Alex-Coode <estebanbarce24@gmail.com>
2025-02-22 16:55:32 -0300

    Actualizando código

--- index.php
+++ index.php
@@ -19,12 +19,12 @@
 </head>
 
 <body>
-    <div id="login-container">
+    <div id="content">
         <h1>DECATHLON</h1>
-        <form action="php/login_be.php" method="POST"> 
-            <input class="form" type="email" placeholder="Escriba su Correo" name="email_user" required>
-            <input class="form" type="password" placeholder="Escriba su Contraseña" name="password_user" required>
-            <input class="form" type="submit" value="Iniciar Sesión">
+        <form class="login" action="php/login_be.php" method="POST"> 
+            <input class="userData" type="email" placeholder="Correo electrónico" name="email_user" required>
+            <input class="userData" type="password" placeholder="Contraseña" name="password_user" required>
+            <input class="userData bottom" type="submit" value="Iniciar Sesión">
         </form>
 
         <a class="link" href="crear-cuenta.php">Crear cuenta</a>
